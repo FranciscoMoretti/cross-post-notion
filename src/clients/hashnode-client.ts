4a497b0725ad05f987ebcb5a65710fbb95fe3cdb
Francisco Moretti <franciscoemoretti@gmail.com>
2023-07-24 20:43:09 +0100

    use post in hashnode client

--- src/clients/hashnode-client.ts
+++ src/clients/hashnode-client.ts
@@ -1,123 +1,123 @@
-import { GraphQLClient, gql } from 'graphql-request'
-import { HashnodeConnectionSettings, HashnodeOptions, HashnodeProperties } from "../types/clients/hashnode";
-import { ConfigHashnode, ConfigNotion } from "../types/config";
-import Notion from './notion-client';
+import { GraphQLClient, gql } from "graphql-request";
+import {
+  HashnodeConnectionSettings,
+  HashnodeOptions,
+  HashnodeProperties,
+} from "../types/clients/hashnode";
+import { ConfigHashnode } from "../types/config";
+import { Post } from "../types/post";
 
 type HashnodeTag = {
-  _id: string,
-  slug: string,
-  name: string
-}
+  slug: string;
+  _id: string;
+  name: string;
+};
 
 class HashnodeClient {
-  connection_settings: HashnodeConnectionSettings
-  options: HashnodeOptions
-  notion: Notion
-  client: GraphQLClient
+  connection_settings: HashnodeConnectionSettings;
+  options: HashnodeOptions;
+  client: GraphQLClient;
+  postData: Post;
 
-  constructor (
-    config: ConfigHashnode,
-    notion_config: ConfigNotion
-  ) {
-    this.connection_settings = config.connection_settings
-    this.options = config.options || {}
-    this.notion = new Notion(notion_config)
-    
-    this.client = new GraphQLClient('https://api.hashnode.com', {
+  constructor(config: ConfigHashnode, postData: Post) {
+    this.connection_settings = config.connection_settings;
+    this.options = config.options || {};
+    this.postData = postData;
+    this.client = new GraphQLClient("https://api.hashnode.com", {
       headers: {
-        authorization: this.connection_settings.token
-      }
-    })
+        authorization: this.connection_settings.token,
+      },
+    });
   }
 
-  async post (url: string, dryRun?: boolean) {
-    //get page id
-    const pageId = this.notion.getPageIdFromURL(url)
-    //get blocks
-    const blocks = await this.notion.getBlocks(url)
-
-    //transform blocks to markdown
-    const markdown = await this.notion.getMarkdown(blocks)
-    const properties = await this.notion.getArticleProperties(pageId)
-
-    const canonical_url = this.notion.getAttributeValue(properties[this.options.properties?.original_article_url || HashnodeProperties.ORIGINAL_ARTICLE_URL])
-
+  async post(url: string, dryRun?: boolean) {
     //get tags
-    let tags: HashnodeTag[] = []
-    const notionTags = this.notion.getAttributeValue(properties[this.options.properties?.tags || HashnodeProperties.TAGS])
-    if (notionTags) {
-      tags = await this.getTagsFromHashnode(notionTags.split(",").map((tag) => tag.trim()))
+    let hashNodeTags: HashnodeTag[] = [];
+    const inputTags = this.postData.tags;
+    if (inputTags) {
+      hashNodeTags = await this.getTagsFromHashnode(
+        inputTags.split(",").map((tag) => tag.trim())
+      );
     }
 
     const createStoryInput = {
-      title: 'My Article Title',
-      contentMarkdown: markdown,
+      title: "My Article Title",
+      contentMarkdown: this.postData.markdown,
       // Use description as subtitle if they are 150 chars
       // subtitle: this.notion.getAttributeValue(properties[this.options.properties?.subtitle || HashnodeProperties.SUBTITLE]),
-      ...(canonical_url && {
+      ...(this.postData.canonical_url && {
         isRepublished: {
-          originalArticleUrl: canonical_url
-        }
+          originalArticleUrl: this.postData.canonical_url,
+        },
       }),
-      tags,
+      tags: hashNodeTags,
       isPartOfPublication: {
-        publicationId: this.connection_settings.publication_id
-      }
-    }
-
+        publicationId: this.connection_settings.publication_id,
+      },
+    };
 
     //post to personal
     const mutation = gql`
-      mutation createPublicationStory($input: CreateStoryInput!, $publicationId: String!, $hideFromHashnodeFeed: Boolean!) {
-        createPublicationStory(input: $input, publicationId: $publicationId, hideFromHashnodeFeed: $hideFromHashnodeFeed) {
-          success,
+      mutation createPublicationStory(
+        $input: CreateStoryInput!
+        $publicationId: String!
+        $hideFromHashnodeFeed: Boolean!
+      ) {
+        createPublicationStory(
+          input: $input
+          publicationId: $publicationId
+          hideFromHashnodeFeed: $hideFromHashnodeFeed
+        ) {
+          success
           message
         }
       }
-    `
-    
+    `;
+
     const data = {
       input: createStoryInput,
       publicationId: this.connection_settings.publication_id,
-      hideFromHashnodeFeed: this.options.should_hide
-    }
+      hideFromHashnodeFeed: this.options.should_hide,
+    };
 
     if (dryRun) {
-      console.log('No error occurred while preparing article for Hashnode.')
-      return
+      console.log("No error occurred while preparing article for Hashnode.");
+      return;
     }
 
-    await this.client.request(mutation, data)
+    await this.client.request(mutation, data);
 
-    console.log('Article pushed to Hashnode')
+    console.log("Article pushed to Hashnode");
   }
 
-  async getTagsFromHashnode (tags: string[]): Promise<HashnodeTag[]> {
-    const hashnodeTags: HashnodeTag[] = []
+  async getTagsFromHashnode(tags: string[]): Promise<HashnodeTag[]> {
+    const hashnodeTags: HashnodeTag[] = [];
     //retrieve all tags from hashnode
     const query = gql`
       {
         tagCategories {
-          _id,
-          name,
+          _id
+          name
           slug
         }
       }
-    `
+    `;
 
-    const response = await this.client.request(query)
+    const response = await this.client.request(query);
 
     tags.forEach((tag) => {
       //find tag in the response
-      const hashnodeTag = response.tagCategories?.find((t: HashnodeTag) => t.name === tag || t.slug === tag)
+      const hashnodeTag = response.tagCategories?.find(
+        (t: HashnodeTag) => t.name === tag || t.slug === tag
+      );
 
       if (hashnodeTag) {
-        hashnodeTags.push(hashnodeTag)
+        hashnodeTags.push(hashnodeTag);
       }
-    })
+    });
 
-    return hashnodeTags
+    return hashnodeTags;
   }
 }
 
-export default HashnodeClient
+export default HashnodeClient;
